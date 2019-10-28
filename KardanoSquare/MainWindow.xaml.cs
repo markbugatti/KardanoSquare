bcad0c11f172de8f82f887f4e85e70becaac3597
Mark <supermark11@gmail.com>
2019-10-28 21:03:06 +0200

    Add project files.

---
+++ KardanoSquare/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KardanoSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int[,] stencil;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int column = Grid.GetColumn(button);
+            int row = Grid.GetRow(button);
+            if((string)button.Content == "0")
+            {
+                button.Content = "1";
+                stencil[row, column] = 1;
+            }
+            else
+            {
+                button.Content = "0";
+                stencil[row, column] = 0;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            if (sizeTextBox.Text != "")
+            {
+                int size = Int32.Parse(sizeTextBox.Text);
+                stencilHandler.Draw(stencilStackPanel, size);
+                stencil = new int[size, size];
+            }
+        }
+    }
+}
---
+++ KardanoSquare/stencilHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KardanoSquare
+{
+    class stencilHandler
+    {
+        const int gridLenght = 20;
+        public static void Draw(StackPanel stackPanel, int size)
+        {
+            Grid grid = new Grid();
+            grid.HorizontalAlignment = HorizontalAlignment.Center;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    RowDefinition row = new RowDefinition();
+                    row.Height = new GridLength(gridLenght);
+                    ColumnDefinition column = new ColumnDefinition();
+                    column.Width = new GridLength(gridLenght);
+
+                    
+                    grid.RowDefinitions.Add(row);
+                    grid.ColumnDefinitions.Add(column);
+
+                    Button button = new Button();
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+                    button.Click += MainWindow.Button_Click;
+                    TextBlock text = new TextBlock();
+                    text.FontSize = 16;
+                    text.Padding = new Thickness(10);
+                    text.Text = "0";
+                    button.Content = text;
+
+                    grid.Children.Add(button);
+                }
+            }
+            stackPanel.Children.Add(grid);
+        }
+
+
+    }
+}
